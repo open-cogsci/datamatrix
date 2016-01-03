8c6b441be9afb21c08a5137098f8b5a4775aee5f
Sebastiaan Mathot <s.mathot@cogsci.nl>
2016-01-03 15:32:38 +0100

    setup: add travis integration and setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This file is part of zoteromarkdown.
+
+zoteromarkdown is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+zoteromarkdown is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with zoteromarkdown.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from dataframe import __version__
+from setuptools import setup, find_packages
+
+setup(
+	name=u'python-datamatrix',
+	version=__version__,
+	description= u'An intuitive, Pythonic way to work with tabular data',
+	author=u'Sebastiaan Mathôt',
+	author_email=u's.mathot@cogsci.nl',
+	license=u'GNU GPL Version 3',
+	url=u'https://github.com/smathot/python-datamatrix',
+	packages=find_packages('.')
+)
