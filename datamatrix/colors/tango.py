b291de7d908444aa94adb5de50b4b9cc9c9823a4
Sebastiaan Mathot <s.mathot@cogsci.nl>
2016-01-04 17:18:00 +0100

    colors: add tango colors (merged from exparser)

---
+++ datamatrix/colors/tango.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file is part of datamatrix.
+
+datamatrix is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+datamatrix is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with datamatrix.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from datamatrix.py3compat import *
+
+butter = ['#fce94f', '#edd400', '#c4a000']
+orange = ['#fcaf3e', '#f57900', '#ce5c00']
+chocolate = ['#e9b96e', '#c17d11', '#8f5902']
+chameleon = ['#8ae234', '#73d216', '#4e9a06']
+skyblue = ['#729fcf', '#3465a4', '#204a87']
+plum = ['#ad7fa8', '#75507b', '#5c3566']
+scarletred = ['#ef2929', '#cc0000', '#a40000']
+aluminium = ['#eeeeec', '#d3d7cf', '#babdb6', '#888a85', '#555753', '#2e3436']
+yellow = butter
+brown = chocolate
+green = chameleon
+blue = skyblue
+purple = plum
+red = scarletred
+grey = aluminium
+gray = aluminium
+
+allcolors = butter + orange + chocolate + chameleon + skyblue + plum + \
+	scarletred
+
+brightcolors = [butter[0], orange[0], chameleon[0], skyblue[0], plum[0], \
+	scarletred[0]]
