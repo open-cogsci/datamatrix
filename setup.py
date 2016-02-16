536b4cfd33652210975dcf9f025d157de626a0e6
Sebastiaan Mathot <s.mathot@cogsci.nl>
2016-02-16 17:49:26 +0100

    setup: fix old reference to DataFrame

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with zoteromarkdown.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from dataframe import __version__
+from datamatrix import __version__
 from setuptools import setup, find_packages
 
 setup(
