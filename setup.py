a72f4ff7c6e08ffa3c5bc3478067543f3916afee
Sebastiaan Mathot <s.mathot@cogsci.nl>
2016-03-27 13:38:39 +0200

    Update setup for stdeb

--- setup.py
+++ setup.py
@@ -2,30 +2,33 @@
 # -*- coding: utf-8 -*-
 
 """
-This file is part of zoteromarkdown.
+This file is part of datamatrix.
 
-zoteromarkdown is free software: you can redistribute it and/or modify
+datamatrix is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-zoteromarkdown is distributed in the hope that it will be useful,
+datamatrix is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with zoteromarkdown.  If not, see <http://www.gnu.org/licenses/>.
+along with datamatrix.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from datamatrix import __version__
 from setuptools import setup, find_packages
 
+# Increment to force a change in the source tarball.
+DUMMY=1
+
 setup(
 	name=u'python-datamatrix',
 	version=__version__,
 	description= u'An intuitive, Pythonic way to work with tabular data',
-	author=u'Sebastiaan Mathôt',
+	author=u'Sebastiaan Mathot',
 	author_email=u's.mathot@cogsci.nl',
 	license=u'GNU GPL Version 3',
 	packages=find_packages('.', exclude=[u'testcases']),
