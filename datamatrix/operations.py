4de42bf0218e8c724c19a463f88125efdcabe1b3
Sebastiaan Mathot <s.mathot@cogsci.nl>
2017-04-17 16:45:38 +0200

    operations: fix Py3 compatibility in full_factorial()

--- datamatrix/operations.py
+++ datamatrix/operations.py
@@ -686,7 +686,7 @@ def _fullfact(levels):
 	level_repeat = 1
 	range_repeat = np.prod(levels)
 	for i in range(n):
-		range_repeat /= levels[i]
+		range_repeat //= levels[i]
 		lvl = []
 		for j in range(levels[i]):
 			lvl += [j]*level_repeat
