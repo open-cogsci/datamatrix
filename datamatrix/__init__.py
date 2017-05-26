22b84caf853237fdc91ae9d749f5ee2f39171db7
Sebastiaan Mathot <s.mathot@cogsci.nl>
2017-05-26 21:13:05 +0200

    Bump to 0.4.13

--- datamatrix/__init__.py
+++ datamatrix/__init__.py
@@ -26,4 +26,4 @@ from datamatrix._datamatrix._seriescolumn import SeriesColumn
 from datamatrix._datamatrix._datamatrix import DataMatrix
 from datamatrix._cache import cached, iscached
 
-__version__ = '0.4.12'
+__version__ = '0.4.13'
