c1498b57baba5c819b24b5991214528b7f74785d
Sebastiaan Mathot <s.mathot@cogsci.nl>
2018-01-02 11:44:33 +0100

    Index: Use numpy where available to improve performance

--- datamatrix/_datamatrix/_index.py
+++ datamatrix/_datamatrix/_index.py
@@ -26,136 +26,162 @@ except ImportError:
 
 
 class Index(OrderedState):
-	
+
 	"""
 	desc:
 		An index object that resembles a list but is more efficient.
 	"""
-	
-	def __init__(self, start=0):		
-		
+
+	def __init__(self, start=0):
+
 		if isinstance(start, int):
-			self._l = list(range(start))			
+			if np is None:
+				self._l = list(range(start))
+			else:
+				self._l = np.arange(start)
 			self._length = start
 			self._metaindex = None
 			self._max = start-1
 		elif isinstance(start, list):
-			self._l = start
+			if np is None:
+				self._l = start
+			else:
+				self._l = np.array(start)
 			self._length = len(start)
 			self._metaindex = None
-			self._max = None			
+			self._max = None
 		elif isinstance(start, Index):
-			self._l = start._l[:]
+			self._l = start._l.copy()
 			self._length = start._length
 			self._max = start._max
 			self._metaindex = start._metaindex
-		elif isinstance(start, set) \
-				or (np is not None and isinstance(start, np.ndarray)):
-			self._l = list(start)
+		elif (
+			isinstance(start, set)
+			or (np is not None and isinstance(start, np.ndarray))
+		):
+			if np is None:
+				self._l = list(start)
+			elif isinstance(start, set):
+				self._l = np.array(list(start))
+			else:
+				self._l = start.copy()
 			self._length = len(self._l)
 			self._metaindex = None
-			self._max = None			
+			self._max = None
 		else:
 			raise Exception('Invalid Index start: %s' % type(start))
 
 	def __getitem__(self, item):
-				
+
 		if isinstance(item, slice):
 			return Index(self._l[item])
 		if isinstance(item, (tuple, list)):
 			i = Index(0)
 			for row in item:
 				i.append(self._l[row])
-			return i			
+			return i
 		return self._l[item]
-		
+
 	def __setitem__(self, index, item):
-		
+
 		self._l[index] = item
-		
+
 	def __add__(self, other):
-		
-		self._l += other
+
+		if np is None:
+			self._l += other
+		else:
+			self._l = np.concatenate([self._l, other])
 		self._length = len(self._l)
 		self._metaindex = None
 		self._max = None
 		return self
-		
+
 	def __len__(self):
-		
+
 		return self._length
-		
+
 	def __iter__(self):
-		
+
 		for i in self._l:
 			yield i
-			
+
 	def __unicode__(self):
-		
+
 		return u'Index(%s)' % self._l
-		
+
 	def __str__(self):
-		
+
 		return safe_str(self.__unicode__())
-		
+
 	def __repr__(self):
-		
-		return u'%s[0x%x]\n%s' % (self.__class__.__name__, id(self), str(self))		
-			
+
+		return u'%s[0x%x]\n%s' % (self.__class__.__name__, id(self), str(self))
+
 	def __getstate__(self):
-		
+
 		# Is used by pickle.dump. To make sure that Index objects with only a
 		# different _metaindex are considered identical
 		return OrderedState.__getstate__(self, ignore=u'_metaindex')
-		
+
 	def __setstate__(self, state):
-		
+
 		OrderedState.__setstate__(self, state)
-		# Start with a _metaindex=None after unpickling		
+		# Start with a _metaindex=None after unpickling
 		self._metaindex = None
-			
+
 	def index(self, i):
-		
+
 		if self._metaindex is None:
-			self._metaindex = \
-				{rowid : index for index, rowid in enumerate(self._l)}
+			self._metaindex = {
+				rowid : index
+				for index, rowid
+				in enumerate(self._l)
+			}
 		return self._metaindex[i]
-		
+
 	def append(self, i):
-		
-		self._l.append(i)
+
+		if np is None:
+			self._l.append(i)
+		else:
+			self._l = np.concatenate([self._l, [i]])
 		self._length += 1
 		self._metaindex = None
 		if i > self._max:
 			self._max = i
-		
+
 	def clone(self):
-		
+
 		i = Index(0)
-		i._l = self._l[:]
+		i._l = self._l.copy()
 		i._max = self._max
 		i._length = self._length
 		return i
-		
+
 	@property
 	def max(self):
-		
+
 		if self._max is not None:
 			return self._max
 		self._max = max(self._l)
 		return self._max
-		
+
 	@property
 	def asarray(self):
-				
+
 		if np is None:
 			raise Exception('numpy is not available')
-		return np.array(self._l, dtype=int)
+		#return np.array(self._l, dtype=int)
+		return self._l
 
 	def sorted(self):
-		
+
 		i = Index(0)
-		i._l = sorted(self._l[:])
+		if np is None:
+			i._l = sorted(self._l)
+		else:
+			i._l = np.sort(self._l)
 		i._max = self._max
 		i._length = self._length
 		return i
