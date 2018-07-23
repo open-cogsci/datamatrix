93916dd07f16d1ef9d7a1a0bac95502575f5c795
Sebastiaan Mathot <s.mathot@cogsci.nl>
2018-07-23 11:24:35 +0200

    Bump to 0.9.4

--- datamatrix/__init__.py
+++ datamatrix/__init__.py
@@ -26,6 +26,6 @@ from datamatrix._datamatrix._seriescolumn import SeriesColumn
 from datamatrix._datamatrix._datamatrix import DataMatrix
 from datamatrix._cache import cached, iscached
 
-__version__ = '0.9.3'
+__version__ = '0.9.4'
 NAN = float('nan')
 INF = float('inf')
