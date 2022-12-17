d92da6b07f73d315e96021643f6ae65607ad204d
smathot <s.mathot@cogsci.nl>
2022-12-17 13:06:31 +0100

    Update memmap test

--- testcases/test_memmap.py
+++ testcases/test_memmap.py
@@ -30,6 +30,10 @@ def _memmap_dm(x=1):
     for i in it.count():
         print(f'creating column {i}')
         dm[f'm{i}'] = MultiDimensionalColumn(shape=(100, 100))
+        # At least one column should be loaded, otherwise the test doens't
+        # make sense.
+        if i == 0 and not dm[f'm{i}'].loaded:
+            return None
         dm[f'm{i}'] = i * x
         for j in range(i):
             if not dm[f'm{j}'].loaded:
@@ -52,27 +56,30 @@ def test_dynamic_loading():
     cfg.min_mem_free_rel = .6
     cfg.min_mem_free_abs = INF
     dm = _memmap_dm()
-    print('without touching')
-    for name, col in dm.columns:
-        print(name, col.loaded)
-    assert not dm.m0.loaded
-    print('setting m0')
-    dm.m0 = -1
-    assert dm.m0[..., ..., ...] == -1
-    for name, col in dm.columns:
-        print(name, col.loaded)
-    assert dm.m0.loaded
-    print('getting m1')
-    dm.m1 = -1
-    assert dm.m1[..., ..., ...] == -1
-    for name, col in dm.columns:
-        print(name, col.loaded)
-    assert dm.m1.loaded
-    print('operating m2')
-    assert (dm.m2 * 2)[..., ..., ...] == 4
-    for name, col in dm.columns:
-        print(name, col.loaded)
-    assert dm.m2.loaded
+    if dm is None:
+        print('insufficient memory to run test')
+    else:
+        print('without touching')
+        for name, col in dm.columns:
+            print(name, col.loaded)
+        assert not dm.m0.loaded
+        print('setting m0')
+        dm.m0 = -1
+        assert dm.m0[..., ..., ...] == -1
+        for name, col in dm.columns:
+            print(name, col.loaded)
+        assert dm.m0.loaded
+        print('getting m1')
+        dm.m1 = -1
+        assert dm.m1[..., ..., ...] == -1
+        for name, col in dm.columns:
+            print(name, col.loaded)
+        assert dm.m1.loaded
+        print('operating m2')
+        assert (dm.m2 * 2)[..., ..., ...] == 4
+        for name, col in dm.columns:
+            print(name, col.loaded)
+        assert dm.m2.loaded
     cfg.min_mem_free_rel = .5
     cfg.min_mem_free_abs = 4294967296
     cfg.always_load_max_size = 134217728
@@ -86,12 +93,15 @@ def test_memmap_writebin():
     cfg.min_mem_free_rel = .6
     cfg.min_mem_free_abs = INF
     test_dm = _memmap_dm()
-    print('writing binary file')
-    io.writebin(test_dm, 'tmp.dm')
-    print('reading binary file')
-    ref_dm = io.readbin('tmp.dm')
-    print('checking datamatrix objects')
-    check_dm(test_dm, ref_dm)
+    if test_dm is None:
+        print('insufficient memory to run test')
+    else:
+        print('writing binary file')
+        io.writebin(test_dm, 'tmp.dm')
+        print('reading binary file')
+        ref_dm = io.readbin('tmp.dm')
+        print('checking datamatrix objects')
+        check_dm(test_dm, ref_dm)
     cfg.min_mem_free_rel = .5
     cfg.min_mem_free_abs = 4294967296
     cfg.always_load_max_size = 134217728
@@ -104,7 +114,10 @@ def test_memmap_multiprocess_stack():
     cfg.always_load_max_size = 0
     cfg.min_mem_free_rel = .6
     cfg.min_mem_free_abs = INF
-    dm = fnc.stack_multiprocess(_memmap_dm, [1, 2])
+    if _memmap_dm() is None:
+        print('insufficient memory to run test')
+    else:
+        dm = fnc.stack_multiprocess(_memmap_dm, [1, 2])
     cfg.min_mem_free_rel = .5
     cfg.min_mem_free_abs = 4294967296
     cfg.always_load_max_size = 134217728
