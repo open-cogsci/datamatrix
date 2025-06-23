a69d8231396f0968edb11fd7eca39f77f7ada1cc
smathot <s.mathot@cogsci.nl>
2025-06-23 12:18:32 +0200

    Bump to 2.0.0a2

--- datamatrix/__init__.py
+++ datamatrix/__init__.py
@@ -27,6 +27,6 @@ from datamatrix._datamatrix._multidimensionalcolumn import \
 from datamatrix._datamatrix._nifticolumn import NiftiColumn
 from datamatrix._datamatrix._datamatrix import DataMatrix
 
-__version__ = '2.0.0a1'
+__version__ = '2.0.0a2'
 NAN = float('nan')
 INF = float('inf')
